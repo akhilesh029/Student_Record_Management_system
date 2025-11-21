5e0281b20b18ecde2aea4edee7515ef556baae4a
akhilesh029 <akhilesheka0100@gmail.com>
2025-11-21 13:45:15 +0530

    Updated env variables, backend routes, and deployment config

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,11 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
+
 mongoose
   .connect(db_url)
   .then(() => {
